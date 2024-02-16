442e354e69cb3c9d70aa09bc4526a73cd9809c0f
Ümmü Gülsüm Özaslan <glsmozsln@gmail.com>
2024-02-16 22:51:18 +0300

    Add project files.

---
+++ Business/Concrete/PersonManager.cs
@@ -0,0 +1,38 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PersonManager : IApplicantService   //Vatandaşlar
+    {
+        public void ApplyForMask(Person person)
+        {
+
+        }
+
+        public List<Person> GetList()
+        {
+            return null;
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            //mernis ile doğrulama yapıldı
+
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+
+            return client.TCKimlikNoDogrulaAsync(person.NationalIdentity,person.FirstName,person.LastName, person.DateOfBirthYear).Result.Body.TCKimlikNoDogrulaResult;
+
+
+
+        }
+
+
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Business.Concrete;
+using Entities.Concrete;
+
+Person person1 = new Person();
+person1.FirstName = "Ümmü Gülsüm";
+person1.LastName = "Özaslan";
+person1.NationalIdentity = 11111111111;
+person1.DateOfBirthYear = 1998;
+
+//PersonManager personManager = new PersonManager();
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person1);
